f8ee42a57c2285b08176b8dd93f5ec0ce8999480
sadat25 <sadatshahriarrafin@gmail.com>
2025-11-23 22:28:02 +0600

    s

---
+++ JS/Script.js
@@ -0,0 +1,12 @@
+// code for time 
+
+const d = new Date();
+let text = d.toLocaleString();
+document.getElementById("demo").innerHTML = text;
+
+// code for popup
+
+function showAlert() {
+    alert("Message Sent Successfully!");
+}
+
